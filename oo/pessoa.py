1e2f77dd11bb6c15e6f7fa8de0e91c9ac4ab7f90
alan_mariquito <alan.mariquito@gmail.com>
2021-04-07 15:05:52 -0300

    Criado atributos de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=37):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return 'Olá'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Batata')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = "Alan"
+    print(p.nome)
+    print(p.idade)
